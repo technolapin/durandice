9fe3cb087d6f95430957cac67bced424a616e8ee
technolapin <clement.chomicki@protonmail.com>
2020-11-13 11:36:40 +0100

    added rlisp and database

--- src/commands.rs
+++ src/commands.rs
@@ -1,17 +1,52 @@
 #[derive(Debug)]
 pub enum Command
 {
-    Roll(u64, u64),
+    Roll(i64, i64),
     Warhammer
     {
-        score: u64,
+        score: i64,
     },
-    Pouet
+    Pouet,
+    Coupable,
+    Shadowrun(i64, Option<i64>),
+    Degenesis(i64, Option<i64>),
+    Trudvang(i64, i64, i64),
+    Help,
+    Say(String),
+    Brigandine
+    {
+        explode_tresh: i64,
+    },
+    CmdList(Vec<Self>),
+    CommentedCmd(String, Box<Self>),
 }
 
-fn dice(n_faces: u64) -> u64
+pub fn dice(n_faces: i64) -> i64
 {
-    rand::random::<u64>() % n_faces + 1
+    (rand::random::<u64>() % (n_faces as u64) + 1) as i64
+}
+
+fn explode<F>(n_faces: i64,
+              condition: F,
+              depth: usize) -> Result<Vec<i64>, String>
+where
+    F: Fn(i64)-> bool
+{
+    if depth > 1000
+    {
+        return Err(format!("nan mais t'abuse là avec ton dé explosif"))
+    }
+    let r = dice(n_faces);
+    let mut v = vec![r];
+    if !condition(r)
+    {
+        Ok(v)
+    }
+    else
+    {
+        v.append(&mut explode(n_faces, condition, depth+1)?);
+        Ok(v)
+    }
 }
 
 impl Command
@@ -20,6 +55,92 @@ impl Command
     {
         match self
         {
+            Self::Help =>
+            {
+                String::from(r#"
+#Syntaxe
+Les espaces sont ignorés, mettez en autant que vous voulez (ou pas du tout)
+Un paramètre du type <n> est obligatoire
+Un paramètre du type [n] est optionel
+
+## Commande commentée
+'commentaire': <n'importe quelle autre commande>
+
+## liste de commandes
+<commande 1> ; <commande 2> ; ...
+
+## Dés normaux
+commande: "d" ou "roll"
+
+"?[nb de dés]d<nb de faces>"
+
+Exemples:
+
+?4d100
+?d100
+?10000d0 => oui bon
+
+## Jets de warhammer v4
+commande: "warhammer" OU "wh"
+
+"?wh <score>"
+
+Exemples:
+
+?wh 64 => un jet sous 64
+?wh 100 => un jet de CC à -30 pour Rudolf
+
+## Shadowrun
+commande: "sr" OU "shadowrun"
+
+"?sr <score> [difficulté]"
+
+Exemple:
+
+?sr 10 => un jet à 10 dés
+?sr 10 3 => un jet à 10 dés où on doit atteindre 3 réussites
+
+## Degenesis
+commande: "dg" OU "dege" OU "degenesis"
+
+"?dg <score> [difficulté]"
+
+Exemple:
+
+?dg 10 => un jet à 10 dés
+?dg 17 => un jet à 12 dés plus 5 réussites automatiques
+?dg 5 3 => un jet à 5 dés où on doit atteindre 3 réussites
+
+## Trudvang
+commande: "trv" OU "trudvang" OU "trud"
+
+"?trv[score] [JO <seuil>] [bonus/malus/bon/mal <bonus ou malus>]"
+
+Exemples:
+
+?trv => 1d10 tout simple qui n'explose pas
+?trv 3 => 3d10 qui n'explosent pas
+?trv JO 10 => 1d10 qui explose sur 10
+?trv 4 JO 5 => 4d10 qui explosent sur 5 ou plus
+?trv JO 0 => la mort du respect
+?trv 4 JO 5 BON 2=> 4d10 qui explosent sur 5 ou plus avec un bonus de 2
+?trv 4 JO 5 MAL 2=> 4d10 qui explosent sur 5 ou plus avec un malus de 2
+
+
+## Brigandine
+commande: "brig" ou "br" ou "brigandine"
+
+"?br [seuil d'explosion]"
+
+Exemples:
+
+?br => jet de brigandine qui explose sur les 10
+?br 9 => jet de brigandine qui explose sur 9+ 
+?br 6 => jet de brigandine qui explose sur 6+ 
+?br 1 => non
+
+"#)
+            },
             Self::Roll(sides, n) =>
             {
                 let mut out = String::new();
@@ -33,14 +154,238 @@ impl Command
             Self::Warhammer{score} =>
             {
                 let d = dice(100);
-                if d <= score
+                if d % 11 == 0
                 {
-                    format!("{}: Réussite de {} degrés", d, score/10 - d/10)
+                    if d <= score
+                    {
+                        format!("{}: Réussite critique de {} degrés", d, score/10 - d/10)
+                    }
+                    else
+                    {
+                        format!("{}: Échec critique de {} degrés", d, d/10 - score/10)
+                    }
+                    
                 }
                 else
                 {
-                    format!("{}: Échec de {} degrés", d, d/10 - score/10)
+                    if d <= score
+                    {
+                        format!("{}: Réussite de {} degrés", d, score/10 - d/10)
+                    }
+                    else
+                    {
+                        format!("{}: Échec de {} degrés", d, d/10 - score/10)
+                    }
+
                 }
+            },
+            Self::Coupable =>
+            {
+                format!("Jovial.")
+            },
+            Self::Shadowrun(n, maybe_goal) =>
+            {
+                let mut dies = (0..n).map(|_| dice(6)).collect::<Vec<_>>();
+                dies.sort();
+
+                let n_success = dies.iter().filter(|&&a| a >= 5).count() as i64;
+                let n_ones =  dies.iter().filter(|&&a| a == 1).count() as i64;
+
+                let throw_s = dies.iter()
+                    .fold(String::new(), |s, n| format!("{} {}", s, n));
+
+                let complication_m = if n_ones > n_success
+                {format!("Complication - ")}
+                else
+                {String::new()};
+                
+                let msg = if let Some(goal) = maybe_goal
+                {
+                    if goal <= n_success
+                    {
+                        format!("Réussite ({}/{})", n_success, goal)
+                    }
+                    else if n_ones > n_success
+                    {
+                        format!("Échec critique! ({}/{})", n_success, goal)
+                    }
+                    else
+                    {
+                        format!("Échec ({}/{})", n_success, goal)
+                    }
+                }
+                else
+                {
+                    if n_ones > n_success
+                    {
+                        format!("{} réussites (Risque d'échec critique)", n_success)
+                    }
+                    else
+                    {
+                        format!("{} réussites", n_success)
+                    }
+
+                };
+
+                format!("[{}]\n{}{}", throw_s, complication_m, msg)
+                
+            },
+            Self::Degenesis(n, maybe_goal) =>
+            {
+                let mut dies = (0..n.min(12)).map(|_| dice(6)).collect::<Vec<_>>();
+                dies.sort();
+                let n_auto = n - dies.len() as i64;
+                let n_success = dies.iter().filter(|&&a| a >= 4).count() as i64 + n_auto;
+                let n_trigg = dies.iter().filter(|&&a| a == 6).count() as i64;
+                let n_ones =  dies.iter().filter(|&&a| a == 1).count() as i64;
+
+                let throw_s = dies.iter()
+                    .fold(String::new(), |s, n| format!("{} {}", s, n));
+
+                let m_auto = if n_auto == 0 {String::new()} else {format!("({} automatiques) ", n_auto)};
+                let m_triggers = if n_trigg == 0 {String::new()} else {format!("dont {} triggers", n_trigg)};
+                let m_bilan = format!("{} réussites {}{}", n_success, m_auto, m_triggers);
+                let m_analyse = if let Some(goal) = maybe_goal
+                {
+                    if n_success >= goal
+                    {
+                        format!("Réussite ({} sur {})", n_success, goal)
+                    }
+                    else if n_ones > n_success
+                    {
+                        format!("Échec critique!")
+                    }
+                    else
+                    {
+                        format!("Échec")
+                    }
+
+                }
+                else
+                {
+                    if n_ones > n_success
+                    {
+                        format!("Possibilité d'échec critique")
+                    }
+                    else
+                    {
+                        format!("")
+                    }
+                    
+                };
+                
+                format!("[{}]\n{}\n{}", throw_s, m_bilan, m_analyse)
+                
+                
+            },
+            Self::Trudvang(n, expl_tresh, bonus) =>
+            {
+                let mut dices = Vec::new();
+                dices.resize_with(
+                    n as usize,
+                    || {explode(10, |n| n >= expl_tresh, 0)}
+                );
+
+                if let Some(Err(err)) = dices.iter()
+                    .find(|maybe| maybe.is_err())
+                {
+                    format!("{}", err)
+                }
+                else
+                {
+                    let dices = dices.iter().map(|maybe| maybe.clone().unwrap()).collect::<Vec<_>>();
+                    let sum = dices.iter().flatten()
+                        .fold(0i64, |sum, die| sum + die) + bonus;
+                    let throw_m = dices.iter()
+                        .map(|v|
+                             {
+                                 v.iter()
+                                     .fold(String::new(),
+                                           |s, n| format!("{} {}", s, n))
+                             }
+                        )
+                        .fold(String::new(),
+                              |out, s| format!("[{}] {}", s, out)
+                        );
+                    let total_m = format!("Total: {}", sum);
+                    format!("{}\n{}", throw_m, total_m)
+
+                }
+            },
+            Self::Say(s) =>
+            {
+                s.clone()
+            },
+            Self::Brigandine{explode_tresh} =>
+            {
+                let mut units = vec![];
+                match explode(10, |n| n >= explode_tresh, 0)
+                {
+                    Err(err) => {return err;},
+                    Ok(mut explosion) =>
+                    {
+                        units.append(
+                            &mut explosion
+                        );
+                    }
+                }
+
+                
+                let unit_dice = units[0];
+                let tenth_dice = dice(10);
+                let d100 = (tenth_dice % 10)*10 + (unit_dice % 10);
+                let inverse = (unit_dice % 10)*10 + (tenth_dice % 10);
+                println!("inverse: {}", inverse);
+                let hit_location = match inverse
+                {
+                    1..=9 => "Tête",
+                    10 => "Main gauche",
+                    11..=24 => "Bras gauche",
+                    25 => "Main droite",
+                    26..=44 => "Bras droit",
+                    45..=69 => "Torse",
+                    70..=80 => "Abdomen",
+                    81..=88 => "Jambe gauche",
+                    89 => "Pied gauche",
+                    90..=99 => "Jambe droite",
+                    100 => "Pied droit",
+                    _ => unreachable!()
+                };
+                
+
+                let damages: i64 = units.iter().sum();
+                
+                let throw_m = units[1..].iter()
+                    .fold(format!("{}", units[0]), |s, n| format!("{}+{}", s, n));
+                let explode_m = if units.len() == 1
+                {String::new()}
+                else
+                {
+                    if dice(100) == 1
+                    {
+                        format!("(explosion: {}) Macron Explosion!", throw_m)
+                    }
+                    else
+                    {
+                        format!("(explosion: {})", throw_m)
+                    }
+                };
+                let location_m = format!("localisation: {}", hit_location);
+                format!("test: {} dégâts: {}  {}\n{}", d100, damages, explode_m, location_m)
+                
+                
+            },
+
+            Self::CmdList(commands) =>
+            {
+               commands.into_iter()
+                    .fold(format!(""),
+                          |s, cmd| format!("{}===================================\n{}\n", s, cmd.execute())
+                    )
+            },
+            Self::CommentedCmd(com, cmd) =>
+            {
+                format!("##{}\n{}", com, (*cmd).execute())
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,142 @@
 use std::env::args;
-
-
-
+use rlisp::Lisp;
 use serenity::{
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
+use rusqlite::{Connection, Result};
+use rusqlite::NO_PARAMS;
+use chrono::Utc;
+use std::convert::From;
+
 #[macro_use] extern crate lalrpop_util;
 lalrpop_mod!(pub parser);
 
 
-struct Handler;
+
+/*
+struct LastCommand;
+
+impl TypeMapKey for LastCommand
+{
+    type Value = Arc<RwLock<HashMap<String, String>>>;
+}
+
+struct Database;
+
+impl TypeMapKey for Database
+{
+    type Value = Arc<Connection>;
+}
+*/
+
+enum BotOutput
+{
+    Text(String),
+    Image{
+        url: String,
+        maybe_text: Option<String>
+    },
+    Nothing
+}
+
+struct Handler{}
+
+impl Handler
+{
+    fn new() -> Self
+    {
+        Self{}
+    }
+}
 
 impl EventHandler for Handler
 {
     fn message(&self, ctx: Context, msg: Message) {
-        
         if msg.content.starts_with("?")
 	{
-	    let text = msg.content.get(1..).unwrap();
-            let username = msg.author.name;
-            let out = match parser::CmdParser::new().parse(text)
+            let realname = String::from(&msg.author.name);
+            let username = if let Some(member) = msg.member(&ctx.cache)
             {
-                Ok(cmd) =>
+                if let Some(nick) = member.nick
                 {
-                    let result = cmd.execute();
-                    format!("```Markdown\n# {}\n{}\n```", username, result)
-                },
+                    nick.clone()
+                }
+                else
+                {
+                    msg.author.name
+                }
+            }
+            else
+            {
+                msg.author.name
+            };
+            
+            let conn = Connection::open("database.db").unwrap();
+
+	    let mut text = String::from(&msg.content[1..].to_ascii_lowercase());
+
+
+            println!(r#"ENTRY: {} entered "{}""#, realname, text);
+
+            let out = parse(text, username, realname, &conn);
+
+            match out
+            {
                 Err(err) =>
                 {
-                    format!("syntax error ({})", err)
-                }
-            };
+                    let content = format!("Error: {:?}", err);
+                    if let Err(why) = msg.channel_id.say(&ctx.http, &content)
+	            {
+                        println!("Error sending error message {:?} ({:?})", content, why);
+                    }
 
+                },
+                Ok(answer) => match answer
+                {
+                    BotOutput::Nothing =>
+                    {
+                        println!("Bot output: No output");
+                    },
+                    BotOutput::Text(content) =>
+                    {
+                        if let Err(why) = msg.channel_id.say(&ctx.http, &content)
+	                {
+                            println!("Error sending message: {:?}", why);
+                        }
+                        else
+                        {
+                            println!(r#"Bot output: Text "{}""#, content);
+                        }
+                    },
+                    BotOutput::Image{url, maybe_text} =>
+                    {
+                        if let Err(why) = msg.channel_id
+                            .send_files(&ctx.http,
+                                        vec![url.as_str()],
+                                        |m| {  if let Some(text) = maybe_text.clone()
+                                               {m.content(&text)}
+                                               else
+                                               {m.content("")} })
+	                {
+                            println!("Error sending message: {:?}", why);
+                        }
+                        else
+                        {
+                            println!("Bot output: Image (url: {}, text: {:?})", url, maybe_text);
+                        }
+                    }
+                }
+                
+            }
+            /*
             if let Err(why) = msg.channel_id.say(&ctx.http, &out)
 	    {
                 println!("Error sending message: {:?}", why);
             }
+             */
 
+            
         }
     }
 
@@ -47,7 +146,199 @@ impl EventHandler for Handler
     }
 }
 
-fn main()
+fn parse(text: String,
+         username: String,
+         realname: String,
+         conn: &Connection) -> Result<BotOutput, Error>
+{
+    let maybe_first_word = text.split_whitespace().nth(0);
+
+    // do nothing if no non-whitespace char
+    if maybe_first_word.is_none()
+    {
+        return Ok(BotOutput::Nothing);
+    }
+    // safe to unwrap
+    let first_word = maybe_first_word.unwrap();
+
+    match first_word
+    {
+        "re" =>
+        {
+            let name = text.split_whitespace().nth(1).unwrap_or("__last_entry");
+
+            let mut stmt = conn.prepare(
+                "SELECT username, command_name, command, date FROM registered_commands
+                     WHERE username=(?1) AND command_name=(?2)
+                     ORDER BY date DESC;"
+            ).unwrap();
+
+            let cmd: Option<String> = stmt.query_map(&[&realname, name], |row| {
+                row.get(2)
+            }).unwrap().filter_map(|result| result.ok()).next();
+
+            if let Some(ancient) = cmd
+            {
+                parse(ancient, username, realname, conn)
+            }
+            else
+            {
+                Ok(BotOutput::Text(format!("No previous entry for {}", realname)))
+            }
+
+        },
+        "haha" =>
+        {
+            Ok(BotOutput::Image{
+                url: String::from("assets/haha_nelson.jpg"),
+                maybe_text: Some(format!("Haha!"))
+            })
+        },
+        "list" =>
+        {
+            let mut stmt = conn.prepare(
+                "SELECT username, command_name, command, date FROM registered_commands
+                     WHERE username=(?1)
+                     ORDER BY date DESC;"
+            )?;
+            let entries = stmt.query_map(&[&realname], |row| {
+                Ok((row.get(1)?, row.get(2)?))
+            })?
+                .filter_map(|result| result.ok())
+                .filter_map(|option: (Option<String>, Option<String>)|
+                            match option
+                            {
+                                (Some(name), Some(code)) => Some(format!("{}: {}", name, code)),
+                                _ => None
+                            })
+                .filter_map(|entry| if entry.chars().next() == Some('_') {None} else {Some(entry)})
+                .collect::<Vec<_>>();
+            Ok(BotOutput::Text(format!("```{}```", if entries.len() > 0
+                               {
+                                   entries
+                                       .into_iter()
+                                       .fold(String::new(), |s, entry| format!("{}{}\n", s, entry))
+                               }
+                               else
+                               {
+                                   format!("Pas de macros enregistrées pour {}", realname)
+                               })))
+                
+        },
+        "forget" =>
+        {
+            let name = text.split_whitespace().nth(1)
+                .ok_or(Error::new("bad unwrap 1"))?;
+            conn.execute(
+                "DELETE FROM registered_commands WHERE username = (?1) AND command_name = (?2);",
+                &[&realname, name],
+            )?;
+            
+            Ok(BotOutput::Text(format!("Commande {} de {} supprimée", name, realname)))
+                
+
+        },
+        "register" =>
+        {
+            println!("REGISTER");
+            let code = text[8..].chars()
+                .skip_while(|c| c.is_whitespace())
+                .skip_while(|c| !c.is_whitespace())
+                .collect::<String>();
+
+            let name = text.split_whitespace().nth(1)
+                .ok_or(Error::new("bad unwrap 2"))?;
+            let date = Utc::now().timestamp();
+            conn.execute(
+                "INSERT INTO registered_commands (username, command_name, command, date)
+                     values (?1, ?2, ?3, ?4)",
+                &[&realname, name, &code, &format!("{}", date)],
+            )?;
+            Ok(BotOutput::Text(format!("Commande enregistrée pour {} en tant que '{}'", realname, name)))
+                
+        },
+        // commands that can be registered and such
+        other =>
+        {
+            let date = Utc::now().timestamp();
+            conn.execute(
+                "INSERT INTO registered_commands (username, command_name, command, date)
+                     values (?1, ?2, ?3, ?4)",
+                &[&realname, "__last_entry", &text, &format!("{}", date)],
+            )?;
+
+            match other
+            {
+                "lisp" =>
+                {
+                    let code = &(&text)[4..];
+                    let mut lisp = Lisp::new();
+                    let out = lisp.evaluate(code);
+                    Ok(BotOutput::Text(
+                        match out
+                        {
+                            Ok(result) =>
+                            {
+                                format!("```Markdown\n# {}\n{}\n```", username, result)
+                            },
+                            Err(err) => format!("```Markdown\n# {}\n{:?}\n```", username, err)
+                        }))
+                },
+                _ =>
+                {
+                    let result = Error::convert_result(parser::CmdListParser::new().parse(&text))?.execute();
+                    Ok(BotOutput::Text(format!("```Markdown\n# {}\n{}\n```", username, result)))
+
+                }
+            }
+        }
+    }
+    
+}
+
+
+#[derive(Debug, Clone)]
+struct Error(String);
+
+impl Error
+{
+    pub fn new(msg: &str) -> Self
+    {
+        Self(String::from(msg))
+    }
+
+    pub fn from_other<E: std::fmt::Debug>(err: E) -> Self
+    {
+        Self(format!("{:?}", err))
+    }
+
+    pub fn convert_result<T, E: std::fmt::Debug>(res: Result<T, E>) -> Result<T, Self>
+    {
+        match res
+        {
+            Ok(thing) => Ok(thing),
+            Err(err) => Err(Self::from_other(err))
+        }
+    }
+}
+
+
+
+macro_rules! impl_from {
+    ($t:path) => {
+        impl From<$t> for Error
+        {
+            fn from(err: $t) -> Self
+            {
+                Self(format!("{}", err))
+            }
+        }
+    };
+}
+
+impl_from!(rusqlite::Error);
+
+fn main() -> Result<(), Error>
 {
     let arguments = args().collect::<Vec<_>>();
     if arguments.len() != 2
@@ -57,9 +348,27 @@ fn main()
     
     let token = &arguments[1];
 
-    let mut client = Client::new(&token, Handler).expect("Err creating client");
+    let mut client = Client::new(&token, Handler::new()).expect("Err creating client");
 
+    {
+        let conn = Connection::open("database.db").unwrap();
+
+        conn.execute(
+            "create table if not exists registered_commands (
+             id integer primary key,
+             username text not null,
+             command_name text not null,
+             command text not null,
+             date integer
+             )",
+            NO_PARAMS,
+        ).unwrap();
+        
+//        data.insert::<Database>(conn.clone());
+    }
+    
     if let Err(why) = client.start() {
         println!("Clien error: {:?}", why);
     }
+    Ok(())
 }
